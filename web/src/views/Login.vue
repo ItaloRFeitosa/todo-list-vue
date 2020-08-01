a746ead55396d6353613ebc44804f05f65498aa2
ItaloRFeitosa <italo85199@gmail.com>
2020-08-01 15:16:26 -0300

    Initial Commit

---
+++ web/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login-container">
+    <div class="login-content">
+      <h1>Welcome to this TODO App</h1>
+      <h4>Please, tell me your name</h4>
+      <input type="text" placeholder="Your Name" />
+      <button>Let's Start ></button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
