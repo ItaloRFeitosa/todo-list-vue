240200ef2870a0f7f294a96319294c65205c6652
ItaloRFeitosa <italo85199@gmail.com>
2020-08-02 19:56:25 -0300

    Add global state management with vuex

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,23 +1,55 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { setTasks, getTasks } from "../services/tasks.mock.api";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    tasks: [
-      {
-        id: "a5v836w5gf45n5a",
-        title: "Aprender VueJS",
-        description:
-          "Aprender os principais conceitos de vue, vuex e vue-router até segunda",
-        date: new Date(),
-        done: false
-      }
-    ]
+    tasks: []
   },
+  getters: {
+    allTasks(state) {
+      return state.tasks;
+    },
 
-  mutations: {},
-  actions: {},
+    notDoneTasksCount(state) {
+      return state.tasks.filter(task => !task.done).length;
+    },
+
+    doneTasks(state) {
+      return state.tasks.filter(task => task.done);
+    },
+
+    notDoneTasks(state) {
+      return state.tasks.filter(task => !task.done);
+    }
+  },
+  mutations: {
+    updateTasks(state, tasks) {
+      state.tasks = tasks;
+    }
+  },
+  actions: {
+    toggleDoneTask({ commit, state }, id) {
+      const tasks = [...state.tasks];
+      const toggledTask = tasks.find(task => task.id === id);
+      toggledTask.done = !toggledTask.done;
+      setTasks(tasks);
+      commit("updateTasks", tasks);
+    },
+
+    addTask({ commit, state }, task) {
+      const updatedTasks = [...state.tasks, task];
+      setTasks(updatedTasks);
+      commit("updateTasks", updatedTasks);
+    },
+
+    getTasksFromStorage({ commit }) {
+      const tasks = getTasks();
+      commit("updateTasks", tasks);
+    }
+  },
   modules: {}
 });
