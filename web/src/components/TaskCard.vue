bdae46866e4f209565dd2900d0c498d8b95cc5e9
ItaloRFeitosa <italo85199@gmail.com>
2020-08-02 22:26:42 -0300

    Change style

--- web/src/components/TaskCard.vue
+++ web/src/components/TaskCard.vue
@@ -79,7 +79,7 @@ $gray: #aaa;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 8px;
+  padding: 12px;
   width: 100%;
   background: #fff;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
@@ -90,7 +90,7 @@ $gray: #aaa;
     h4 {
       font-weight: bold;
       color: $base-color;
-      margin-bottom: 4px;
+      margin-bottom: 8px;
     }
     span {
       color: $gray;
